3282f1c1132f218bc876ddbc02abdb3d4abaa833
JonathanSovisa <jonathan.sovisa@si.ukdw.ac.id>
2024-10-02 03:47:00 +0700

    first commit

---
+++ Data/IStudent.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleSecureWeb.Models;
+
+namespace SampleSecureWeb.Data
+{
+    public interface IStudent
+    {
+    IEnumerable<Student> GetStudents();
+    Student GetStudent(string Nim);
+    Student AddStudent(Student student);
+    Student UpdateStudent(Student student);
+    void DeleteStudent (string nim);
+    
+      
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleSecureWeb.Models;
+
+public class Student
+{
+
+public int Id { get; set; }
+public String Nim { get; set; } = null!;
+public String FullName { get; set; } = null!;
+}
